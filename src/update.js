efd05e60e21b4cb2f4cf01e1e55c64e6f9d5626e
Danil Semelenov <mail@danil.mobi>
2019-01-30 13:44:26 +0300

    Review a script

--- src/update.js
+++ src/update.js
@@ -1,9 +1,11 @@
 import Kefir from '../node_modules/kefir/dist/kefir.esm.js';
 import state from './state.js';
 
-let update;
-Kefir.stream(emitter => (update = emitter.emit))
-  .scan((state, patch) => patch(state), state)
-  .map(state => console.log('state', state))
-  .observe();
-export default update;
+let emit;
+const stream = Kefir.stream(emitter => (emit = emitter.emit)).scan(
+  (state, patch) => patch(state),
+  state,
+);
+
+export const onState = callback => stream.observe(callback);
+export default state => emit(state);
