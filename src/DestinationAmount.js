b729c27db87ff7fe49d030dc458a7b066ce78521
Danil Semelenov <mail@danil.mobi>
2019-01-31 01:12:08 +0300

    Format a value in DestinationAmount

--- src/DestinationAmount.js
+++ src/DestinationAmount.js
@@ -1,4 +1,5 @@
 import exchangeRate from './exchange-rate.js';
+import formatCurrency from './format-currency.js';
 import html from './html.js';
 
 let text;
@@ -6,11 +7,15 @@ export default (props, state) => {
   const hidden = state.ratesHidden || !state.amount;
   text = hidden
     ? text
-    : `+${exchangeRate(
-        state.rates,
-        state.sourcePocket.currency,
+    : `+${formatCurrency(
+        exchangeRate(
+          state.rates,
+          state.sourcePocket.currency,
+          state.destinationPocket.currency,
+        ) * state.amount,
         state.destinationPocket.currency,
-      ) * state.amount}`;
+        { currencyDisplay: 'code' },
+      ).replace(/[A-Z]{3}/, '')}`;
   return html`
     <span class="DestinationAmount animated" hidden=${hidden}>${text}</span>
   `;
