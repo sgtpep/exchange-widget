cf2170106bfc12a6107825a8b07ce5ae65898842
Danil Semelenov <mail@danil.mobi>
2019-01-31 00:41:19 +0300

    Implement ReversedRate

--- src/ReversedRate.js
+++ src/ReversedRate.js
@@ -1,3 +1,16 @@
 import html from './html.js';
+import rateText from './rate-text.js';
 
-export default props => 'ReversedRate';
+let prevText;
+export default (props, state) => {
+  prevText = state.ratesHidden
+    ? prevText
+    : rateText(
+        state.rates,
+        state.destinationPocket.currency,
+        state.sourcePocket.currency,
+      );
+  return html`
+    <span class="animated" hidden=${state.ratesHidden}>${prevText}</span>
+  `;
+};
