afe6b28acf44aea07323918ac08bce8278fd8ae2
Danil Semelenov <mail@danil.mobi>
2019-01-30 21:04:02 +0300

    Memoize actual exchange rates

--- src/exchange-rate.js
+++ src/exchange-rate.js
@@ -1,9 +1,26 @@
-const rate = (rates, currency) =>
+const currencyRate = (rates, currency) =>
   (rates.find(rate => rate.currency === currency) || {}).rate;
 
-export default (rates, fromCurrency, toCurrency) =>
+const exchangeRate = (rates, fromCurrency, toCurrency) =>
   fromCurrency === 'USD'
-    ? rate(rates, toCurrency)
+    ? currencyRate(rates, toCurrency)
     : toCurrency === 'USD'
-    ? 1 / rate(rates, fromCurrency)
-    : rate(rates, toCurrency) * (1 / rate(rates, fromCurrency));
+    ? 1 / currencyRate(rates, fromCurrency)
+    : currencyRate(rates, toCurrency) * (1 / currencyRate(rates, fromCurrency));
+
+const cache = new Map();
+let prevRates;
+export default (rates, fromCurrency, toCurrency) => {
+  if (rates !== prevRates) {
+    cache.clear();
+    prevRates = rates;
+  }
+  const key = `${fromCurrency}:${toCurrency}`;
+  if (cache.has(key)) {
+    return cache.get(key);
+  } else {
+    const result = exchangeRate(rates, fromCurrency, toCurrency);
+    cache.set(key, result);
+    return result;
+  }
+};
