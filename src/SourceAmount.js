11a3d4a18160ec15da161aa010ad2417ced72aab
Danil Semelenov <mail@danil.mobi>
2019-01-30 18:41:19 +0300

    Preserve a focus an input on sliding

--- src/SourceAmount.js
+++ src/SourceAmount.js
@@ -1,15 +1,31 @@
 import html from './html.js';
+import { Component, createRef } from '../node_modules/preact/dist/preact.mjs';
 import { setAmount } from './actions.js';
 
-export default (props, state) => html`
-  <span>
-    ${state.amount && '-'}
-    <input
-      max=${props.max}
-      min="1"
-      onInput=${event => setAmount(event.target.value)}
-      type="number"
-      value=${state.amount}
-    />
-  </span>
-`;
+export default class extends Component {
+  componentDidUpdate(props, _, state) {
+    setTimeout(() => this.input.current.focus());
+  }
+
+  constructor() {
+    super();
+    this.input = createRef();
+  }
+
+  render(props, _, state) {
+    return html`
+      <span>
+        ${state.amount && '-'}
+        <input
+          autofocus
+          max=${props.max}
+          min="1"
+          onInput=${event => setAmount(event.target.value)}
+          ref=${this.input}
+          type="number"
+          value=${state.amount}
+        />
+      </span>
+    `;
+  }
+}
