288149d0ce60c4cc0274941c877b510063a7bb46
Danil Semelenov <mail@danil.mobi>
2019-01-30 19:35:45 +0300

    Style hidden <select> in RateDropdown

--- src/RateDropdown.js
+++ src/RateDropdown.js
@@ -1,3 +1,11 @@
 import html from './html.js';
 
-export default () => 'RateDropdown';
+export default () =>
+  html`
+    <span class="RateDropdown">
+      <select
+        ><option>rate 1</option
+        ><option>rate 2</option></select
+      ><span>current rate</span>
+    </span>
+  `;
