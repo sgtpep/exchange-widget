f3db36ebaa339345c41cf4c80417ee5a0a55f91f
Danil Semelenov <mail@danil.mobi>
2019-01-30 19:38:03 +0300

    Reorder elements

--- src/RateDropdown.js
+++ src/RateDropdown.js
@@ -3,9 +3,10 @@ import html from './html.js';
 export default () =>
   html`
     <span class="RateDropdown">
-      <select
-        ><option>rate 1</option
-        ><option>rate 2</option></select
-      ><span>current rate</span>
+      <span>current rate</span>
+      <select>
+        <option>rate 1</option>
+        <option>rate 2</option>
+      </select>
     </span>
   `;
