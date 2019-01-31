69d1aca97dd0cea8e2b874607f9c6bc415e0305f
Danil Semelenov <mail@danil.mobi>
2019-01-31 18:44:46 +0300

    Extract a component

--- src/RateDropdown.js
+++ src/RateDropdown.js
@@ -1,7 +1,7 @@
+import RateSelect from './RateSelect.js';
 import animatedHideable from './animated-hideable.js';
 import html from './html.js';
 import rateText from './rate-text.js';
-import { setDestinationPocket, setSourcePocket } from './actions.js';
 
 export default animatedHideable(
   (props, state) => state.ratesHidden,
@@ -17,46 +17,7 @@ export default animatedHideable(
     html`
       <span class=${`RateDropdown animated ${props.hidden ? 'hidden' : ''}`}>
         <span>${props.text}</span>
-        <select
-          onChange=${event => {
-            const [
-              sourceCurrency,
-              destinationCurrency,
-            ] = event.target.value.split(':');
-            setSourcePocket(
-              state.pockets.find(pocket => pocket.currency === sourceCurrency),
-            );
-            setDestinationPocket(
-              state.pockets.find(
-                pocket => pocket.currency === destinationCurrency,
-              ),
-            );
-          }}
-        >
-          ${state.pockets.map(sourcePocket =>
-            state.pockets.map(
-              destinationPocket =>
-                destinationPocket.currency === sourcePocket.currency ||
-                html`
-                  <option
-                    selected=${destinationPocket === state.destinationPocket &&
-                      sourcePocket === state.sourcePocket}
-                    value=${`${sourcePocket.currency}:${
-                      destinationPocket.currency
-                    }`}
-                  >
-                    ${!state.rates.length ||
-                      rateText(
-                        state.rates,
-                        sourcePocket.currency,
-                        destinationPocket.currency,
-                        { minimumFractionDigits: 4 },
-                      )}
-                  </option>
-                `,
-            ),
-          )}
-        </select>
+        <${RateSelect} />
       </span>
     `,
 );
---
+++ src/RateSelect.js
@@ -0,0 +1,41 @@
+import html from './html.js';
+import rateText from './rate-text.js';
+import { setDestinationPocket, setSourcePocket } from './actions.js';
+
+export default (props, state) => html`
+  <select
+    onChange=${event => {
+      const [sourceCurrency, destinationCurrency] = event.target.value.split(
+        ':',
+      );
+      setSourcePocket(
+        state.pockets.find(pocket => pocket.currency === sourceCurrency),
+      );
+      setDestinationPocket(
+        state.pockets.find(pocket => pocket.currency === destinationCurrency),
+      );
+    }}
+  >
+    ${state.pockets.map(sourcePocket =>
+      state.pockets.map(
+        destinationPocket =>
+          destinationPocket.currency === sourcePocket.currency ||
+          html`
+            <option
+              selected=${destinationPocket === state.destinationPocket &&
+                sourcePocket === state.sourcePocket}
+              value=${`${sourcePocket.currency}:${destinationPocket.currency}`}
+            >
+              ${!state.rates.length ||
+                rateText(
+                  state.rates,
+                  sourcePocket.currency,
+                  destinationPocket.currency,
+                  { minimumFractionDigits: 4 },
+                )}
+            </option>
+          `,
+      ),
+    )}
+  </select>
+`;
