60a88b46fae7fdd45cea0a1448568381f3402e63
Danil Semelenov <mail@danil.mobi>
2019-02-01 00:06:32 +0300

    Move the debug code outside of the widget logic

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import App from './App.js';
 import html from './html.js';
 import { render } from '../node_modules/preact/dist/preact.mjs';
 
-export default (element, options) => {
+export default (element, options, onDestroy = undefined) => {
   const root = render(
     html`
       <${App}
         ...${options}
-        destroy=${() =>
-          render(
-            html`
-              <p>Widget destroyed. <a href="">Reload</a></p>
-            `,
-            element,
-            root,
-          )}
+        destroy=${() => {
+          render(null, element, root);
+          onDestroy && onDestroy();
+        }}
       />
     `,
     element,
