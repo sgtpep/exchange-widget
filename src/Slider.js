bcca04b0da824393dbbce5f26d1ca37f5d23d09e
Danil Semelenov <mail@danil.mobi>
2019-01-31 23:23:46 +0300

    Improve a slider

--- src/Slider.js
+++ src/Slider.js
@@ -2,13 +2,15 @@ import html from './html.js';
 import { Component, createRef } from '../node_modules/preact/dist/preact.mjs';
 
 export default class extends Component {
+  activatePage(index) {
+    [...this.pagination.current.querySelectorAll('.active')].forEach(element =>
+      element.classList.remove('active'),
+    );
+    const element = this.pagination.current.children[index];
+    element && element.classList.add('active');
+  }
+
   componentDidMount() {
-    this.props.onMount &&
-      this.props.onMount(
-        this.props.index,
-        this.slides.current.children[this.props.index + 1],
-      );
-    this.selectSlide(this.props.index);
     this.listeners = [
       ['mousedown', this.onDragStart],
       ['mousemove', this.onDragMove],
@@ -20,6 +22,12 @@ export default class extends Component {
     this.listeners.forEach(args =>
       this.slides.current.addEventListener(...args),
     );
+    this.selectSlide(this.props.index);
+    this.props.onMount &&
+      this.props.onMount(
+        this.props.index,
+        this.slides.current.children[this.props.index + 1],
+      );
   }
 
   componentWillUnmount() {
@@ -30,6 +38,7 @@ export default class extends Component {
 
   constructor() {
     super();
+    this.pagination = createRef();
     this.slides = createRef();
   }
 
@@ -66,19 +75,13 @@ export default class extends Component {
     if (event.type === 'touchend' || this.dragging) {
       const { offsetLeft } = this.slides.current;
       const threshold = 100;
-      offsetLeft - this.offsetLeft < -threshold &&
-      this.index <= this.slides.current.children.length - 1
+      offsetLeft - this.offsetLeft < -threshold
         ? this.goToSlide(this.index + 1)
-        : offsetLeft - this.offsetLeft > threshold && this.index >= 0
+        : offsetLeft - this.offsetLeft > threshold
         ? this.goToSlide(this.index - 1)
         : this.goToSlide(this.index);
       this.dragging = false;
     }
-  }
-
-  onSlide(index) {
-    this.props.onSlide &&
-      this.props.onSlide(index, this.slides.current.children[index + 1]);
   }
 
   render(props) {
@@ -107,10 +110,10 @@ export default class extends Component {
             )}
           </div>
         </div>
-        <nav>
+        <nav ref=${this.pagination}>
           ${props.children.map((child, index) => [
             html`
-              <a onClick=${() => this.onSlide(index)}>${index + 1}</a>
+              <a onClick=${() => this.goToSlide(index)}>${index + 1}</a>
             `,
             ' ',
           ])}
@@ -120,8 +123,16 @@ export default class extends Component {
   }
 
   selectSlide(index) {
+    const prevIndex = this.index;
     this.index = index;
     this.slides.current.style.left = `${-(index + 1) * 100}%`;
+    this.activatePage(index);
+    prevIndex === undefined ||
+      prevIndex === index ||
+      (index >= 0 &&
+        index <= this.slides.current.children.length - 3 &&
+        this.props.onSlide &&
+        this.props.onSlide(index, this.slides.current.children[index + 1]));
   }
 
   startAnimation() {
