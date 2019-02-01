ed7a0275d824c0847f525508ec799bd3fc44831e
Danil Semelenov <mail@danil.mobi>
2019-02-01 16:14:01 +0300

    Add an inital rollup config

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+export default {
+  output: [
+    {
+      file: './dist/exchange-widget.esm.min.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: './dist/exchange-widget.umd.min.js',
+      format: 'umd',
+      name: 'exchange-widget',
+      sourcemap: true,
+    },
+  ],
+  input: './src/index.js',
+};
