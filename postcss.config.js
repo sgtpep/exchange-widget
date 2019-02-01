16f9589822e5e747d244240f02dab831acd62262
Danil Semelenov <mail@danil.mobi>
2019-02-01 18:10:17 +0300

    Remove postcss-prefix-selector

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,6 @@ module.exports = ctx => ({
   map: ctx.options.map,
   plugins: {
     'postcss-import': {},
-    'postcss-prefix-selector': {
-      exclude: [/^(:root|body|html|\.App)($|[^\w-])/],
-      prefix: '.App',
-    },
     autoprefixer: {},
   },
 });
