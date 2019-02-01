2248cf4036eb0fb987929e5b5430a2426baecebe
Danil Semelenov <mail@danil.mobi>
2019-02-01 17:48:04 +0300

    Generate only one script

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,16 @@ import babel from 'rollup-plugin-babel';
 import commonJS from 'rollup-plugin-commonjs';
 import inject from 'rollup-plugin-inject';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: './src/index.js',
-  output: [
-    {
-      file: './dist/exchange-widget.es.min.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: './dist/exchange-widget.min.js',
-      format: 'iife',
-      name: 'exchangeWidget',
-      sourcemap: true,
-    },
-    {
-      file: './dist/exchange-widget.umd.min.js',
-      format: 'umd',
-      name: 'exchange-widget',
-      sourcemap: true,
-    },
-  ],
+  output: {
+    file: './dist/exchange-widget.min.js',
+    format: 'iife',
+    name: 'exchangeWidget',
+    sourcemap: true,
+  },
   plugins: [
     babel({
       ignore: ['./node_modules'],
