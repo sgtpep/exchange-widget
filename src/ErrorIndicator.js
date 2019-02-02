fd6c0c80b1944ca20d090fd267499a4149531b9e
Danil Semelenov <mail@danil.mobi>
2019-02-02 22:18:31 +0300

    Remove an unnecessary check

--- src/ErrorIndicator.js
+++ src/ErrorIndicator.js
@@ -4,8 +4,8 @@ import { Component } from '../node_modules/preact/dist/preact.mjs';
 export default class extends Component {
   componentWillReceiveProps(props) {
     if (props.visible) {
-      this.base.classList.remove('hidden'),
-        this.hidingTimeout && clearTimeout(this.hidingTimeout);
+      this.base.classList.remove('hidden');
+      clearTimeout(this.hidingTimeout);
       this.hidingTimeout = setTimeout(
         () => this.base.classList.add('hidden'),
         3000,
