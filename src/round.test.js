a9574dc10d922d0b17a72750e14caedcf1084639
Danil Semelenov <mail@danil.mobi>
2019-02-03 08:51:23 +0300

    Remove newlines, change messages

--- src/round.test.js
+++ src/round.test.js
@@ -1,7 +1,5 @@
 require = require('esm')(module);
 const round = require('./round');
 
-test('rounds 0.5 to 1', () => expect(round(0.5)).toBe(1));
-
-test('rounds 0.005 with two decimals to 0.01', () =>
-  expect(round(0.005, 2)).toBe(0.01));
+test('round 0.5', () => expect(round(0.5)).toBe(1));
+test('round 0.005 with two decimals', () => expect(round(0.005, 2)).toBe(0.01));
