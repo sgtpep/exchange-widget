9b919b7764d9a1ee662e6e1f310d96d138f022f3
Danil Semelenov <mail@danil.mobi>
2019-02-03 11:09:27 +0300

    Add a test

---
+++ src/rate-text.test.js
@@ -0,0 +1,11 @@
+require = require('esm')(module);
+const rateText = require('./rate-text');
+
+const rates = [{ currency: 'EUR', rate: 0.9 }];
+
+test('rate text', () =>
+  expect(rateText(rates, 'USD', 'EUR')).toBe('$1 = €0.90'));
+test('rate text with more decimals', () =>
+  expect(rateText(rates, 'USD', 'EUR', { minimumFractionDigits: 4 })).toBe(
+    '$1 = €0.9000',
+  ));
