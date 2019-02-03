6f7a24877676f60dd906a3289c8096b0e599e959
Danil Semelenov <mail@danil.mobi>
2019-02-04 02:17:30 +0300

    Add a test

---
+++ src/ErrorIndicator.test.js
@@ -0,0 +1,33 @@
+const { shallow } = require('preact-render-spy');
+
+require = require('esm')(module);
+const ErrorIndicator = require('./ErrorIndicator');
+const html = require('./html');
+
+test('show an error indicator', () => {
+  expect(
+    shallow(
+      html`
+        <${ErrorIndicator} visible>error<//>
+      `
+    ).output()
+  ).toEqual(
+    html`
+      <div class="ErrorIndicator animated"><div>error</div></div>
+    `
+  );
+});
+
+test('hide an error indicator', () => {
+  expect(
+    shallow(
+      html`
+        <${ErrorIndicator}>error<//>
+      `
+    ).output()
+  ).toEqual(
+    html`
+      <div class="ErrorIndicator animated hidden"><div>error</div></div>
+    `
+  );
+});
