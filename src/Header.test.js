3735a74faa69c69e787bec677d02456aa1b3d551
Danil Semelenov <mail@danil.mobi>
2019-02-04 08:35:11 +0300

    Add a test

---
+++ src/Header.test.js
@@ -0,0 +1,56 @@
+const fetchMock = require('fetch-mock');
+const stateMock = require('./state-mock');
+
+require = require('esm')(module);
+const Header = require('./Header');
+const html = require('./html');
+
+afterEach(() => fetchMock.reset());
+
+let actions, context, render;
+let destroy, exchange;
+beforeEach(() => {
+  [destroy, exchange] = [jest.fn(), jest.fn()];
+  ({ actions, context, render } = stateMock(
+    html`
+      <${Header} destroy=${destroy} exchange=${exchange} />
+    `
+  ));
+});
+
+describe('exchange button disableness', () => {
+  const button = () => context.find('button').at(1);
+
+  test('check initial disableness of the exchange button', () =>
+    expect(button().attr('disabled')).toBeTruthy());
+
+  test('enable the exchange button', async () => {
+    fetchMock.getOnce('rates', require('../mocks/rates'));
+    await actions.fetchRates('rates');
+    render();
+    expect(button().attr('disabled')).toBeTruthy();
+    actions.setAmount(100);
+    render();
+    expect(button().attr('disabled')).toBeFalsy();
+  });
+});
+
+test('click on the cancel button', async () => {
+  context
+    .find('button')
+    .at(0)
+    .simulate('click');
+  expect(destroy).toBeCalled();
+});
+
+test('click on the exchange button', async () => {
+  fetchMock.getOnce('rates', require('../mocks/rates'));
+  await actions.fetchRates('rates');
+  actions.setAmount(100);
+  context
+    .find('button')
+    .at(1)
+    .simulate('click');
+  render();
+  expect(exchange).toBeCalled();
+});
