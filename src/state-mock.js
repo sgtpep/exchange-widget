0961f2b551520a6eb6e9bcae15ced866fbf83af9
Danil Semelenov <mail@danil.mobi>
2019-02-04 08:20:18 +0300

    Add a helper for testing components dependent on a state

---
+++ src/state-mock.js
@@ -0,0 +1,29 @@
+const { deep } = require('preact-render-spy');
+
+require = require('esm')(module);
+const html = require('./html');
+
+module.exports = (children, depth = 2) => {
+  const [actions, State] = ['./actions', './State'].map(id => {
+    delete require.cache[require.resolve(id)];
+    return require(id);
+  });
+  const node = html`
+    <${State}
+      pockets=${[
+        { currency: 'GBP', sum: 100 },
+        { currency: 'EUR', sum: 200 },
+        { currency: 'USD', sum: 300 },
+      ]}
+    >
+      ${children}
+    <//>
+  `;
+  const context = deep(node, { depth });
+  return {
+    actions,
+    context,
+    node,
+    render: () => context.render(node),
+  };
+};
