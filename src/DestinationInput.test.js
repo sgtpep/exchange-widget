27b3bf3691fd168e858f294afcef2a65f57977e0
Danil Semelenov <mail@danil.mobi>
2019-02-04 10:22:25 +0300

    Add a test

---
+++ src/DestinationInput.test.js
@@ -0,0 +1,67 @@
+const fetchMock = require('fetch-mock');
+const stateMock = require('./state-mock');
+
+require = require('esm')(module);
+const CurrencyInput = require('./CurrencyInput');
+const DestinationInput = require('./DestinationInput');
+const html = require('./html');
+
+afterEach(() => fetchMock.reset());
+
+let actions, context, output, render;
+beforeEach(
+  () =>
+    ({ actions, context, output, render } = stateMock(
+      html`
+        <${DestinationInput} pocket=${{ currency: 'USD', sum: 100 }} />
+      `
+    ))
+);
+
+test('check if the destination input is initially hidden', () =>
+  expect(output()).toMatchInlineSnapshot(`
+<span class="DestinationInput animated hidden">
+  <default
+    prefix=""
+    setAmount={[Function anonymous]}
+    tabindex="-1"
+  >
+  </default>
+</span>
+`));
+
+test('show the destination input', async () => {
+  fetchMock.getOnce('rates', require('../mocks/rates'));
+  await actions.fetchRates('rates');
+  render();
+  expect(output()).toMatchInlineSnapshot(`
+<span class="DestinationInput animated">
+  <default
+    max={130.78973457531265}
+    prefix=""
+    setAmount={[Function anonymous]}
+    tabindex="-1"
+  >
+  </default>
+</span>
+`);
+});
+
+test('set the destination input value by the amount of money for exchange', async () => {
+  fetchMock.getOnce('rates', require('../mocks/rates'));
+  await actions.fetchRates('rates');
+  actions.setAmount(100);
+  render();
+  expect(output()).toMatchInlineSnapshot(`
+<span class="DestinationInput animated">
+  <default
+    max={130.78973457531265}
+    prefix="+"
+    setAmount={[Function anonymous]}
+    tabindex="-1"
+    value={130.79}
+  >
+  </default>
+</span>
+`);
+});
