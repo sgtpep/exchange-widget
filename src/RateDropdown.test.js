d69d34c2a339803a411a384859ab85bc310ccda2
Danil Semelenov <mail@danil.mobi>
2019-02-04 09:06:31 +0300

    Add a test

---
+++ src/RateDropdown.test.js
@@ -0,0 +1,42 @@
+const fetchMock = require('fetch-mock');
+const stateMock = require('./state-mock');
+
+require = require('esm')(module);
+const RateDropdown = require('./RateDropdown');
+const html = require('./html');
+
+afterEach(() => fetchMock.reset());
+
+let actions, context, output, render;
+beforeEach(
+  () =>
+    ({ actions, context, output, render } = stateMock(
+      html`
+        <${RateDropdown} />
+      `
+    ))
+);
+
+test('check if the rate dropdown is hidden initially', () =>
+  expect(output()).toEqual(
+    html`
+      <span class="RateDropdown animated hidden">
+        <span class="button">${''}</span>
+        <${expect.any(Function)}/>
+      </span>
+    `
+  ));
+
+test('display a rate on the rate dropdown', async () => {
+  fetchMock.getOnce('rates', require('../mocks/rates'));
+  await actions.fetchRates('rates');
+  render();
+  expect(output()).toEqual(
+    html`
+      <span class="RateDropdown animated">
+        <span class="button">£1 = €1.1437</span>
+        <${expect.any(Function)}/>
+      </span>
+    `
+  );
+});
