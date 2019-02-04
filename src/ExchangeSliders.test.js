6328e9c8a4491e83953603976c44b08ed9df1503
Danil Semelenov <mail@danil.mobi>
2019-02-04 10:53:08 +0300

    Add a test

---
+++ src/ExchangeSliders.test.js
@@ -0,0 +1,93 @@
+const stateMock = require('./state-mock');
+const { shallow } = require('preact-render-spy');
+
+require = require('esm')(module);
+const ExchangeSliders = require('./ExchangeSliders');
+const html = require('./html');
+
+let actions, context, output, render;
+beforeEach(
+  () =>
+    ({ actions, context, output, render } = stateMock(
+      html`
+        <${ExchangeSliders} />
+      `
+    ))
+);
+
+test('render exchange sliders', () =>
+  expect(output()).toMatchInlineSnapshot(`
+<div class="ExchangeSliders">
+  <default
+    index={0}
+    onMount={[Function anonymous]}
+    onSlide={[Function anonymous]}
+  >
+    <default
+      pocket={
+        Object {
+          "currency": "GBP",
+          "sum": 100
+        }
+      }
+      type="source"
+    >
+    </default>
+    <default
+      pocket={
+        Object {
+          "currency": "EUR",
+          "sum": 200
+        }
+      }
+      type="source"
+    >
+    </default>
+    <default
+      pocket={
+        Object {
+          "currency": "USD",
+          "sum": 300
+        }
+      }
+      type="source"
+    >
+    </default>
+  </default>
+  <default
+    index={1}
+    onSlide={[Function anonymous]}
+  >
+    <default
+      pocket={
+        Object {
+          "currency": "GBP",
+          "sum": 100
+        }
+      }
+      type="destination"
+    >
+    </default>
+    <default
+      pocket={
+        Object {
+          "currency": "EUR",
+          "sum": 200
+        }
+      }
+      type="destination"
+    >
+    </default>
+    <default
+      pocket={
+        Object {
+          "currency": "USD",
+          "sum": 300
+        }
+      }
+      type="destination"
+    >
+    </default>
+  </default>
+</div>
+`));
